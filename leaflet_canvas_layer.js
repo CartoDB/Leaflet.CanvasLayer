9d302a6ad639fe6a06bb6cff2a24614d16a59292
amicenko <andrew.micenko@gmail.com>
2015-08-20 09:43:53 +0800

    Update leaflet_canvas_layer.js
    Changed whitespace back to the original version.

--- leaflet_canvas_layer.js
+++ leaflet_canvas_layer.js
@@ -1,182 +1,182 @@
-if (typeof (L) !== 'undefined') {
-    /**
-     * full canvas layer implementation for Leaflet
-     */
+if(typeof(L) !== 'undefined') {
+/**
+ * full canvas layer implementation for Leaflet
+ */
 
-    L.CanvasLayer = L.Class.extend({
+L.CanvasLayer = L.Class.extend({
 
-        includes: [L.Mixin.Events, L.Mixin.TileLoader],
+  includes: [L.Mixin.Events, L.Mixin.TileLoader],
 
-        options: {
-            minZoom: 0,
-            maxZoom: 28,
-            tileSize: 256,
-            subdomains: 'abc',
-            errorTileUrl: '',
-            attribution: '',
-            zoomOffset: 0,
-            opacity: 1,
-            unloadInvisibleTiles: L.Browser.mobile,
-            updateWhenIdle: L.Browser.mobile,
-            tileLoader: false // installs tile loading events
-        },
+  options: {
+      minZoom: 0,
+      maxZoom: 28,
+      tileSize: 256,
+      subdomains: 'abc',
+      errorTileUrl: '',
+      attribution: '',
+      zoomOffset: 0,
+      opacity: 1,
+      unloadInvisibleTiles: L.Browser.mobile,
+      updateWhenIdle: L.Browser.mobile,
+      tileLoader: false // installs tile loading events
+  },
 
-        initialize: function (options) {
-            options = options || {};
-            this.render = this.render.bind(this);
-            L.Util.setOptions(this, options);
-            this._canvas = this._createCanvas();
-            this._ctx = this._canvas.getContext('2d');
-            this.currentAnimationFrame = -1;
-            this.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-                return window.setTimeout(callback, 1000 / 60);
-            };
-            this.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame ||
-            window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || function (id) { clearTimeout(id); };
-        },
+initialize: function (options) {
+    options = options || {};
+    this.render = this.render.bind(this);
+    L.Util.setOptions(this, options);
+    this._canvas = this._createCanvas();
+    this._ctx = this._canvas.getContext('2d');
+    this.currentAnimationFrame = -1;
+    this.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+                                    return window.setTimeout(callback, 1000 / 60);
+                                };
+    this.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame ||
+                                window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || function(id) { clearTimeout(id); };
+  },
 
-        _createCanvas: function () {
-            var canvas = document.createElement('canvas');
-            canvas.style.position = 'absolute';
-            canvas.style.top = 0;
-            canvas.style.left = 0;
-            canvas.style.pointerEvents = "none";
-            canvas.style.zIndex = this.options.zIndex || 0;
-            var className = 'leaflet-tile-container leaflet-zoom-animated';
-            canvas.setAttribute('class', className);
-            return canvas;
-        },
+  _createCanvas: function() {
+    var canvas = document.createElement('canvas');
+    canvas.style.position = 'absolute';
+    canvas.style.top = 0;
+    canvas.style.left = 0;
+    canvas.style.pointerEvents = "none";
+    canvas.style.zIndex = this.options.zIndex || 0;
+    var className = 'leaflet-tile-container leaflet-zoom-animated';
+    canvas.setAttribute('class', className);
+    return canvas;
+  },
 
-        onAdd: function (map) {
-            this._map = map;
+  onAdd: function (map) {
+    this._map = map;
 
-            // add container with the canvas to the tile pane
-            // the container is moved in the oposite direction of the 
-            // map pane to keep the canvas always in (0, 0)
-            var tilePane = this._map._panes.tilePane;
-            var _container = L.DomUtil.create('div', 'leaflet-layer');
-            _container.appendChild(this._canvas);
-            tilePane.appendChild(_container);
+    // add container with the canvas to the tile pane
+    // the container is moved in the oposite direction of the 
+    // map pane to keep the canvas always in (0, 0)
+    var tilePane = this._map._panes.tilePane;
+    var _container = L.DomUtil.create('div', 'leaflet-layer');
+    _container.appendChild(this._canvas);
+    tilePane.appendChild(_container);
 
-            this._container = _container;
+    this._container = _container;
 
-            // hack: listen to predrag event launched by dragging to
-            // set container in position (0, 0) in screen coordinates
-            if (map.dragging.enabled()) {
-                map.dragging._draggable.on('predrag', function () {
-                    var d = map.dragging._draggable;
-                    L.DomUtil.setPosition(this._canvas, { x: -d._newPos.x, y: -d._newPos.y });
-                }, this);
-            }
+    // hack: listen to predrag event launched by dragging to
+    // set container in position (0, 0) in screen coordinates
+    if (map.dragging.enabled()) {
+      map.dragging._draggable.on('predrag', function() {
+        var d = map.dragging._draggable;
+        L.DomUtil.setPosition(this._canvas, { x: -d._newPos.x, y: -d._newPos.y });
+      }, this);
+    }
 
-            map.on({ 'viewreset': this._reset }, this);
-            map.on('move', this.redraw, this);
-            map.on('resize', this._reset, this);
-            map.on({ 'zoomanim': this._animateZoom }, this);
+    map.on({ 'viewreset': this._reset }, this);
+    map.on('move', this.redraw, this);
+    map.on('resize', this._reset, this);
+    map.on({ 'zoomanim': this._animateZoom }, this);
 
-            if (this.options.tileLoader) {
-                this._initTileLoader();
-            }
+    if(this.options.tileLoader) {
+      this._initTileLoader();
+    }
 
-            this._reset();
-        },
+    this._reset();
+  },
 
-        _animateZoom: function (e) {
-            var zoomPosition = this._map.latLngToContainerPoint(e.center);
-            var mapCentre = this._map.latLngToContainerPoint(this._map.getCenter());
-            var zoomPositionTranslation = ' translate3d(' + (-zoomPosition.x + mapCentre.x) + 'px, ' + (-zoomPosition.y + mapCentre.y) + 'px, 0px)';
-            this._canvas.style.transform = this._canvas.style.transform + ' scale(' + e.scale + ', ' + e.scale + ')' + zoomPositionTranslation;
-        },
+  _animateZoom: function (e) {
+    var zoomPosition = this._map.latLngToContainerPoint(e.center);
+    var mapCentre = this._map.latLngToContainerPoint(this._map.getCenter());
+    var zoomPositionTranslation = ' translate3d(' + (-zoomPosition.x + mapCentre.x) + 'px, ' + (-zoomPosition.y + mapCentre.y) + 'px, 0px)';
+    this._canvas.style.transform = this._canvas.style.transform + ' scale(' + e.scale + ', ' + e.scale + ')' + zoomPositionTranslation;
+  },
 
-        getCanvas: function () {
-            return this._canvas;
-        },
+  getCanvas: function() {
+    return this._canvas;
+  },
 
-        getAttribution: function () {
-            return this.options.attribution;
-        },
+  getAttribution: function() {
+    return this.options.attribution;
+  },
 
-        draw: function () {
-            return this._reset();
-        },
+  draw: function() {
+    return this._reset();
+  },
 
-        onRemove: function (map) {
-            this._container.parentNode.removeChild(this._container);
-            map.off({
-                'viewreset': this._reset,
-                'move': this._render,
-                'resize': this._reset,
-                'zoomanim': this._animateZoom
-            }, this);
-        },
+  onRemove: function (map) {
+    this._container.parentNode.removeChild(this._container);
+    map.off({
+      'viewreset': this._reset,
+      'move': this._render,
+      'resize': this._reset,
+      'zoomanim': this._animateZoom
+    }, this);
+  },
 
-        addTo: function (map) {
-            map.addLayer(this);
-            return this;
-        },
+  addTo: function (map) {
+    map.addLayer(this);
+    return this;
+  },
 
-        setOpacity: function (opacity) {
-            this.options.opacity = opacity;
-            this._updateOpacity();
-            return this;
-        },
+  setOpacity: function (opacity) {
+    this.options.opacity = opacity;
+    this._updateOpacity();
+    return this;
+  },
 
-        setZIndex: function (zIndex) {
-            this._canvas.style.zIndex = zIndex;
-        },
+  setZIndex: function(zIndex) {
+    this._canvas.style.zIndex = zIndex;
+  },
 
-        bringToFront: function () {
-            return this;
-        },
+  bringToFront: function () {
+    return this;
+  },
 
-        bringToBack: function () {
-            return this;
-        },
+  bringToBack: function () {
+    return this;
+  },
 
-        _reset: function () {
-            var size = this._map.getSize();
-            this._canvas.width = size.x;
-            this._canvas.height = size.y;
+  _reset: function () {
+    var size = this._map.getSize();
+    this._canvas.width = size.x;
+    this._canvas.height = size.y;
 
-            // fix position
-            var pos = L.DomUtil.getPosition(this._map.getPanes().mapPane);
-            if (pos) {
-                L.DomUtil.setPosition(this._canvas, { x: -pos.x, y: -pos.y });
-            }
-            this.onResize();
-            this._render();
-        },
+    // fix position
+    var pos = L.DomUtil.getPosition(this._map.getPanes().mapPane);
+    if (pos) {
+      L.DomUtil.setPosition(this._canvas, { x: -pos.x, y: -pos.y });
+    }
+    this.onResize();
+    this._render();
+  },
 
-        _updateOpacity: function () { },
+  _updateOpacity: function () { },
 
-        _render: function () {
-            if (this.currentAnimationFrame >= 0) {
-                this.cancelAnimationFrame.call(window, this.currentAnimationFrame);
-            }
-            this.currentAnimationFrame = this.requestAnimationFrame.call(window, this.render);
-        },
+  _render: function() {
+    if (this.currentAnimationFrame >= 0) {
+      this.cancelAnimationFrame.call(window, this.currentAnimationFrame);
+    }
+    this.currentAnimationFrame = this.requestAnimationFrame.call(window, this.render);
+  },
 
-        // use direct: true if you are inside an animation frame call
-        redraw: function (direct) {
-            var domPosition = L.DomUtil.getPosition(this._map.getPanes().mapPane);
-            if (domPosition) {
-                L.DomUtil.setPosition(this._canvas, { x: -domPosition.x, y: -domPosition.y });
-            }
-            if (direct) {
-                this.render();
-            } else {
-                this._render();
-            }
-        },
+  // use direct: true if you are inside an animation frame call
+  redraw: function(direct) {
+    var domPosition = L.DomUtil.getPosition(this._map.getPanes().mapPane);
+    if (domPosition) {
+      L.DomUtil.setPosition(this._canvas, { x: -domPosition.x, y: -domPosition.y });
+    }
+    if (direct) {
+      this.render();
+    } else {
+      this._render();
+    }
+  },
 
-        onResize: function () {
-        },
+  onResize: function() {
+  },
 
-        render: function () {
-            throw new Error('render function should be implemented');
-        }
+  render: function() {
+    throw new Error('render function should be implemented');
+  }
 
-    });
+});
 
 } //L defined
